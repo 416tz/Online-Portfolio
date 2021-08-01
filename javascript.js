6f5c7575bef082142fc131442ce773a1494e83c0
Taisir <taisirisawesome@gmail.com>
2021-07-31 22:51:13 -0400

    more changes

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function buttonClicked() {
+    if (document.getElementById("namer-container").style.background == "gray") {
+        document.getElementById("namer-container").style.background = "black";
+    } else {
+        document.getElementById("namer-container").style.background = "gray";
+    }
+}
